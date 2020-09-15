0fe1796fbe807dc9ba020c78544c43d3a4885f3c
Allan Nava <allannava95@gmail.com>
2020-09-15 18:08:09 +0200

    Create myobject.js

---
+++ myobject.js
@@ -0,0 +1,14 @@
+/**
+ *  myobject.js
+ *  Allan Nava 2020-09-15
+ *  Allan Nava 2020-09-15
+ *
+ *  Created by [ Allan Nava ].
+ *  Updated by [ Allan Nava ]
+ *  Copyright © 2020 [ Allan Nava ]. All rights reserved.
+*/
+MyObject = {
+  refresh: false,
+  api_url: window.base_api_url != "" ? window.api_url : "https://api.url.it/api/v1/en",
+  //
+}
