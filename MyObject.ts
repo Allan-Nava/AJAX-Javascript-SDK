7f43185f992a7be4401976bb61cd3281735f231d
Allan Nava <allannava95@gmail.com>
2021-05-23 22:13:55 +0200

    Create MyObject.ts

---
+++ MyObject.ts
@@ -0,0 +1,18 @@
+/**
+ *  MyObject.js
+ *  Allan Nava 2020-09-15
+ *  Allan Nava 2020-09-15
+ *
+ *  Created by [ Allan Nava ].
+ *  Updated by [ Allan Nava ]
+ *  Copyright © 2020 [ Allan Nava ]. All rights reserved.
+ * 
+ * 
+ *  Required axios
+ *  https://levelup.gitconnected.com/use-case-of-singleton-with-axios-and-typescript-da564e76296
+*/
+import axios from 'axios';
+//
+class MyObject {
+
+}
